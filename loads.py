eb87650a526d8d8b5619645ac1146fea98c46461
Rodrigo Vega <99046231+VegaRodrigoA@users.noreply.github.com>
2022-04-26 16:31:55 -0300

    Update loads.py

--- loads.py
+++ loads.py
@@ -2,6 +2,8 @@ import sqlite3 as sq
 import pandas as pd
 import yahoo_fin.stock_info as si
 import datetime
+import numpy as np
+
 
 class stock:
     def __init__(self,ticker:str) -> None:
@@ -10,25 +12,14 @@ class stock:
 
     def income(self, tipo: str= "anual"):
         #Obtengo último estado cargado
-        con = sq.connect("/home/rodrigo/Documentos/eFinanc.db")
-        cursor = con.cursor()
-        sql = """SELECT iif((t1.dd ISNULL),"2010-01-01", t1.dd) as """ + tipo + """
-        from Tickers left join
-        (SELECT Ticker, max(fecha) as dd
-        from income GROUP by ticker,tipo
-        HAVING tipo = '""" + tipo +"""' and ticker = '""" + self.ticker + """') as t1 on Tickers.Ticker = t1.ticker
-        WHERE Tickers.ticker = '""" + self.ticker + "';"
-        pk = cursor.execute(sql).fetchone()
-        con.commit()
-        cursor.close()
-        con.close()
-        p = str(pk).replace("(", "").replace(",)", "").replace("'", "")
+        p = funciones("income").ultCargado(self.ticker , self.tipo) 
         #p es el último estado de resultados cargados
         
         if tipo == "anual":
             btipo = True
         else:
             btipo = False
+        
         data=pd.DataFrame(si.get_income_statement(self.ticker,yearly = btipo))
         fecha = max(data.iloc[0,:].index)
         #fecha es último estado de resultados disponible
@@ -38,10 +29,22 @@ class stock:
         if fecha > p:#confirmo si hay algo para cargar
             data.replace(to_replace=[None], value=0, inplace=True)
             indice = data.index#lista con los índices
+            
+            #chequeo de columnas en tabla
+            rdo = funciones("income").checkCol()
+            
             #lista de campos a insertar
             sql1 = "(ticker, fecha,tipo"
             for i in indice:
                 sql1 = sql1 + "," + i
+                if i not in rdo:
+                    con = sq.connect("/home/rodrigo/Documentos/eFinanc.db")
+                    cursor = con.cursor()
+                    sql = "alter TABLE income add " + i +" numeric;;"
+                    cursor.execute(sql)
+                    con.commit()
+                    cursor.close()
+                    con.close()
             
             sql1 = sql1 + ")"
             #print(sql1)
@@ -74,21 +77,9 @@ class stock:
     
     #--------------------Cashflow--------------------------
     def cash(self, tipo: str= "anual"):
-                        #Obtengo último estado cargado
-        con = sq.connect("/home/rodrigo/Documentos/eFinanc.db")
-        cursor = con.cursor()
-        sql = """SELECT iif((t1.dd ISNULL),"2010-01-01", t1.dd) as """ + tipo + """
-        from Tickers left join
-        (SELECT Ticker, max(fecha) as dd
-        from fFondos GROUP by ticker,tipo
-        HAVING tipo = '""" + tipo +"""' and ticker = '""" + self.ticker + """') as t1 on Tickers.Ticker = t1.ticker
-        WHERE Tickers.ticker = '""" + self.ticker + "';"
-        pk = cursor.execute(sql).fetchone()
-        con.commit()
-        cursor.close()
-        con.close()
-        p = str(pk).replace("(", "").replace(",)", "").replace("'", "")
-        #p es el último estado de resultados cargados
+        #Obtengo último estado cargado
+        p = funciones("fFondos").ultCargado(self.ticker,self.tipo)
+        #p es el último estado cargado
         
         if tipo == "anual":
             btipo = True
@@ -102,11 +93,25 @@ class stock:
         
         if fecha > p:#confirmo si hay algo para cargar
             data.replace(to_replace=[None], value=0, inplace=True)
+            data.replace(to_replace=np.nan, value=0, inplace=True)
             indice = data.index#lista con los índices
+
+            #chequeo de columnas en tabla
+            rdo = funciones("fFondos").checkCol()
+            
             #lista de campos a insertar
             sql1 = "(ticker, fecha,tipo"
             for i in indice:
                 sql1 = sql1 + "," + i
+                
+                if i not in rdo:
+                    con = sq.connect("/home/rodrigo/Documentos/eFinanc.db")
+                    cursor = con.cursor()
+                    sql = "alter TABLE fFondos add " + i +" numeric;;"
+                    cursor.executescript(sql)
+                    con.commit()
+                    cursor.close()
+                    con.close()
             
             sql1 = sql1 + ")"
             #print(sql1)
@@ -137,3 +142,36 @@ class stock:
                     except sq.Error as error:
                         print(error)
     
+class funciones:
+    def __init__(self,tabla:str) -> None:
+        self.tabla = tabla
+    
+    def checkCol(self):
+        #chequeo de columnas en tabla
+            import sqlalchemy as db
+            engine = db.create_engine("sqlite:////home/rodrigo/Documentos/eFinanc.db")
+            connection = engine.connect()
+            metadata = db.MetaData()
+            check = db.Table(self.tabla, metadata, autoload=True, autoload_with=engine)
+            rdo = []
+            for i in (check.columns.keys()):
+                rdo.append(i)
+            connection.close()
+            return rdo
+    
+    def ultCargado(self,ticker: str,tipo: str):
+         #Obtengo último estado cargado
+        con = sq.connect("/home/rodrigo/Documentos/eFinanc.db")
+        cursor = con.cursor()
+        sql = """SELECT iif((t1.dd ISNULL),"2010-01-01", strftime('%Y-%m-%d',t1.dd)) as """ + tipo + """
+        from Tickers left join
+        (SELECT Ticker, max(fecha) as dd
+        from """ + self.tabla +""" GROUP by ticker,tipo
+        HAVING tipo = '""" + tipo +"""' and ticker = '""" + ticker + """') as t1 on Tickers.Ticker = t1.ticker
+        WHERE Tickers.ticker = '""" + ticker + "';"
+        pk = cursor.execute(sql).fetchone()
+        con.commit()
+        cursor.close()
+        con.close()
+        p = str(pk).replace("(", "").replace(",)", "").replace("'", "")
+        return p  
